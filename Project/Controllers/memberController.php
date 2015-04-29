b37337888921194fa015e419c7ca4640bbcb9efd
jennie mykkänen <jenniejennie.m@gmail.com>
2015-04-29 15:22:30 +0200

    added showmembercartaction

--- Project/Controllers/memberController.php
+++ Project/Controllers/memberController.php
@@ -21,8 +21,39 @@ class MemberController extends Controller{
         $stm->execute();
         $this->outputJSON($stm->fetch());
     }
+
+    public function getActiveUserCart(){
+        $stm = Database::get()->prepare("SELECT * FROM cart WHERE cart_status = 0");
+        $stm->execute();
+
+        return $stm->fetchAll();
+
+    }
     public function showMemberCartAction(){
-        //TODO Jennie
+        $carts = $this->getActiveUserCart();
+        $cartsContent = [];
+
+        //if there is a cart
+        if (count($carts) > 0){
+
+            foreach($carts as $cart){
+                //select all products from it
+                $stm = Database::get()->prepare("SELECT cart_products.cartproducts_cart_id,
+                                                    products.product_id,
+                                                    products.product_brand,
+                                                    products.product_size,
+                                                    products.product_price,
+                                                    products.product_color
+                        FROM cart_products JOIN products ON cart_products.cartproducts_product_id = products.product_id
+                        WHERE cartproducts_cart_id = :cid");
+
+                $stm->bindParam(":cid", $cart['cart_id']);
+                $stm->execute();
+                $cartsContent[] = $stm->fetchAll();
+            }
+        }
+
+        $this->outputJSON("Here is a complete list of carts", $cartsContent);
     }
 }
 
